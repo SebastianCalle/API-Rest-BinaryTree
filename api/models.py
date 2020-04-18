a87daf3cb1945fb4f40823bc46448c74a5f4aced
SebastianCalle <jusemonca@gmail.com>
2020-04-18 00:04:31 -0500

    [Feature]: Create model for binary tree

--- api/models.py
+++ api/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+import uuid
 
-# Create your models here.
+
+class BinaryTree(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    data = models.CharField()
+    created_at = models.DateTimeField('Created time', auto_now_add=True, null=True)
+
+
