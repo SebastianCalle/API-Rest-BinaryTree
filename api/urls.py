86de05e4effacf004ef14b7ede3f86c340b67d29
SebastianCalle <jusemonca@gmail.com>
2020-04-18 12:13:03 -0500

    [Feature]: Create new view for list all binary trees

--- api/urls.py
+++ api/urls.py
@@ -3,7 +3,8 @@ from rest_framework.urlpatterns import format_suffix_patterns
 from . import views
 
 urlpatterns = [
-    path('', views.apiCreateTree, name='createTree'),
+    path('create', views.apiCreateTree, name='createTree'),
+    path('list', views.apiList, name='apiList'),
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
--- api/views.py
+++ api/views.py
@@ -1,3 +1,47 @@
-from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from .serializers import BinaryTreeSerializer
+from .binary import Code, insert
+from binarytree import Node
+from .models import BinaryTree
 
-# Create your views here.
+
+@api_view(['POST'])
+def apiCreateTree(request):
+    # View for create a Binary Tree
+    data = request.data.split(',')
+    root = Node(int(data[0]))
+    for i in range(1, len(data)):
+        insert(root, Node(int(data[i])))
+    print(root)
+    code = Code()
+    ser = code.serialize(root)
+    binary_dict = {'data': ser}
+    model = BinaryTreeSerializer(data=binary_dict)
+    if model.is_valid():
+        tree = model.save()
+    tree_dict = {
+        'id': tree.id,
+        'data': tree.data,
+        'created': tree.created_at,
+    }
+    return Response(tree_dict)
+
+@api_view(['GET'])
+def apiList(request):
+   tree_list = BinaryTree.objects.all()
+   all_trees = []
+   for tree in tree_list:
+       tree_dict = {
+           'id': tree.id,
+           'data': tree.data,
+           'created': tree.created_at,
+       }
+       all_trees.append(tree_dict)
+
+   return Response(all_trees)
+
+
+#@api_view(['POST'])
+#def apiLowAncestor(request):
+
