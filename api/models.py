c4f5b9fb369712b24c1fbbe18a6d40069e547f27
SebastianCalle <jusemonca@gmail.com>
2020-04-18 11:11:52 -0500

    [Fix]: Change field for data

--- api/models.py
+++ api/models.py
@@ -4,7 +4,7 @@ import uuid
 
 class BinaryTree(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    data = models.CharField()
+    data = models.CharField(max_length=3000)
     created_at = models.DateTimeField('Created time', auto_now_add=True, null=True)
 
 
